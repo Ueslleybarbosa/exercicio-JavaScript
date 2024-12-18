e03c0914085d30f05efc5e370143a80c964244fa
Weslley Barbosa <weslleymaill@gmail.com>
2024-12-18 20:02:19 -0300

    Aprendendo DOM

---
+++ exercício05/script.js
@@ -0,0 +1,14 @@
+var t1 = window.document.querySelector('h1')
+t1.style.color = 'red';
+
+var p1 = window.document.getElementsByTagName('p')[0]
+window.document.write('Esse é o texto - ' + p1.innerText)
+        
+var p2 = window.document.getElementsByTagName('p')[1]
+p2.style.backgroundColor = 'red';
+
+var msg = window.document.querySelector('div#msg')
+msg.style.color = 'blue';
+
+var corpo = window.document.body;
+corpo.style.backgroundColor = 'black'
